21995c84f587181031f0032199f758d906ee8bc8
Juti Noppornpitak <jnopporn@shiroyuki.com>
2013-02-27 00:56:58 -0500

    Bumped up the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name         = 'Imagination',
-    version      = '1.5.7',
+    version      = '1.6a2',
     description  = 'Reusable Component Framework',
     author       = 'Juti Noppornpitak',
     author_email = 'juti_n@yahoo.co.jp',
