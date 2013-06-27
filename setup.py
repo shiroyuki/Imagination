7352d267bb4f2c261aad5fc70bbf93dd97d0ac2c
Juti Noppornpitak <jnopporn@shiroyuki.com>
2013-06-27 12:54:47 -0400

    Updated the version number to 1.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name         = 'Imagination',
-    version      = '1.6',
+    version      = '1.6.1',
     description  = 'Reusable Component Framework',
     author       = 'Juti Noppornpitak',
     author_email = 'juti_n@yahoo.co.jp',
