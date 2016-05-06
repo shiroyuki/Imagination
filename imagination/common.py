9d817b7c02c6a186e0e9714abf18b1865a2f6142
Juti Noppornpitak <juti@statflo.com>
2016-05-06 16:14:33 -0400

    Prevent the reference to be wrapped by Action class if it is already wrapped by Action.

--- imagination/common.py
+++ imagination/common.py
@@ -74,7 +74,7 @@ class InterceptableObject(object):
 
             ref = instance.__getattribute__(attribute)
 
-            if not callable(ref):
+            if not callable(ref) or isinstance(ref, Action):
                 continue
 
             new_ref = Action(ref)
