f9d999e6e6c4c6f42a8c410fb7bce3bc65089376
Juti Noppornpitak <jnopporn@shiroyuki.com>
2016-08-29 22:28:14 -0400

    Bumped up the version to 2.0 (stable)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except:
 
 setup(
     name         = 'imagination',
-    version      = '2.0.0a',
+    version      = '2.0.0',
     description  = 'Reusable Component Framework',
     author       = 'Juti Noppornpitak',
     author_email = 'juti_n@yahoo.co.jp',
